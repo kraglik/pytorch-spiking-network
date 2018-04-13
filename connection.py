7eac85bc899d75f5109c0baf70b9e20d21195c7d
kraglik <igor.kraglik@yandex.ru>
2018-04-13 13:34:55 +0500

    code simplified

--- connection.py
+++ connection.py
@@ -10,21 +10,16 @@ class Connection(nn.Module):
     def __init__(self, pre, post, latency: 1, p=0.5, nu_pre=1e-4, nu_post=1e-2, limit=1.0, plasticity=True):
         super(Connection, self).__init__()
 
-        self.pre = pre
-        self.post = post
+        self.pre = pre                  # Presynaptic neuronal group
+        self.post = post                # Postsynaptic neuronal group
         self.pre.outputs.append(self)
         self.post.inputs.append(self)
         self.nu_pre = nu_pre            # LTP Coefficient
         self.nu_post = nu_post          # LTD Coefficient
         self.limit = limit              # Maximal weight value
-
         self.latency = max(1, latency)
-
         self.plasticity = plasticity
-
-        self.w = torch.rand(pre.size, post.size).float()
-        self.w -= (1.0 - p)
-        self.w = self.w.clamp(0.0, 1.0) * limit
+        self.w = (torch.rand(pre.size, post.size).float() - (1.0 - p)).w.clamp(0.0, 1.0)  # Synaptic weights
 
         self.traces = [
             torch.FloatTensor(pre.size).zero_() for _ in range(self.latency)
@@ -33,17 +28,17 @@ class Connection(nn.Module):
             torch.FloatTensor(pre.size).zero_() for _ in range(self.latency)
         ]
 
-    def forward(self, spikes, dt):
-        current_spikes = self.spikes[-1]
+    def forward(self, pre_spikes, dt):
+        spikes = self.spikes[-1]
         traces = self.traces[-1]
-        self.spikes = [spikes] + self.spikes[:-1]
+        self.spikes = [pre_spikes] + self.spikes[:-1]
         self.traces = [self.pre.spike_traces] + self.traces[:-1]
 
-        output = (self.w.t() * current_spikes).sum(1)
+        output = (self.w.t() * spikes).sum(1)
         self.post.v_i_next += output
 
         if self.plasticity:
-            self.update(traces, current_spikes)
+            self.update(traces, spikes)
 
         return output
 
